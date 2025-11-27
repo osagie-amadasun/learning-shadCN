3fed51789fec070b4261aa9cbc9ee1a188c30a8a
Osagie Amadasun <osagieamadasun123@gmail.com>
2025-11-27 13:21:42 +0100

    initialized git repository

---
+++ components/AppSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppSidebar = () => {
+  return (
+    <div>
+      <h1>App sidebar</h1>
+    </div>
+  )
+}
+
+export default AppSidebar
---
+++ components/Navbar.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
+import { useTheme } from "next-themes";
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* LEFT */}
+      collapse button
+      {/* RIGHT */}
+      <div className="flex items-center gap-4">
+        <Link href={"/"}>Dashboard</Link>
+        {/* THEME MENU */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* USER MENU */}
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            {" "}
+            <Avatar>
+              <AvatarImage src="https://rivalskins.com/wp-content/uploads/marvel-assets/assets/lord-icons/Adam%20Warlock%20Deluxe%20Avatar.png" />
+              <AvatarFallback>CN</AvatarFallback>
+              <span className="sr-only">Open user menu</span>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
